1a3b588b814534863c4fc980029fc81c14f94858
Nemes1sX <kazstonk@gmail.com>
2022-09-07 12:03:56 +0300

    Add ajax call

---
+++ FilterInput.php
@@ -0,0 +1,60 @@
+<?php
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
+            crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+    <script type="text/javascript">
+        $(document).ready(function () {
+           $('#SubmitFilter').click(function () {
+               const url = 'filmsFilter.php';
+               let filmTitle = $('#FilmTitle').val();
+               let filmRunningTime1 = $('#FilmRunningTime1').val();
+               let filmRunningTime2 = $('#FilmRunningTime2').val();
+               let queryString = "?filmtitle=" + filmTitle + "&runningtime1=" + filmRunningTime2 + "&runningtime2=" + filmRunningTime2;
+               console.log([filmTitle, filmRunningTime1, filmRunningTime2]);
+               $.ajax({
+                    type: 'GET',
+                    url: url,
+                    datatype: 'json',
+                    success: function (result) {
+                        console.log(result);
+                    }
+               });
+           })
+        });
+    </script>
+    <title>Film filter input</title>
+</head>
+<body>
+<div class="container">
+    <div class="mb-3">
+        <label for="FilmTitle" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="FilmTitle" placeholder="Example">
+    </div>
+    <div class="mb-3">
+        <label for="FilmRunningTime1" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="FilmRunningTime1" placeholder="90">
+    </div>
+    <div class="mb-3">
+        <label for="FilmRunningTime2" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="FilmRunningTime2" placeholder="120">
+    </div>
+    <button type="submit" class="btn btn-success" id="SubmitFilter">Submit</button>
+    <div id="Results"></div>
+</div>
+</body>
+</html>
--- filmsFilter.php
+++ filmsFilter.php
@@ -6,4 +6,5 @@ $response = json_decode($response);
 $response = array_filter($response, function ($var) {return (stripos($var->title, 'Castle') !== false);  } );
 $response = array_filter($response, function ($var) { return ((int) $var->running_time >= 90 &&
     (int) $var->running_time <= 120); });
-print_r($response);
+
+echo json_encode($response);
